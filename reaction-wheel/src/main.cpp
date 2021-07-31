5497a9455f89492e93f53dfba6450eefc346d2c8
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-07-30 23:05:58 -0500

    create platformio proj

---
+++ reaction-wheel/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <SoftwareSerial.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
